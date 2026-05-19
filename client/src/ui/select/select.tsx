29e13ae84c15ada4ce4ae9b548a0f99e6b59f746
Anton <tonym7@yandex.ru>
2026-05-19 20:14:23 +0300

    Поправил иконки селекта.

--- client/src/ui/select/select.tsx
+++ client/src/ui/select/select.tsx
@@ -24,36 +24,6 @@ interface SelectProps<T extends string> {
   ariaLabel?: string | undefined;
 }
 
-function SelectChevronIcon() {
-  return (
-    <svg className='select__chevron' width='16' height='16' viewBox='0 0 16 16' aria-hidden='true'>
-      <path
-        d='M4 6l4 4 4-4'
-        fill='none'
-        stroke='currentColor'
-        strokeWidth='1.5'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-    </svg>
-  );
-}
-
-function SelectCheckIcon() {
-  return (
-    <svg className='select__check' width='14' height='14' viewBox='0 0 14 14' aria-hidden='true'>
-      <path
-        d='M2.5 7.2 5.4 10.1 11.5 4'
-        fill='none'
-        stroke='currentColor'
-        strokeWidth='1.5'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-    </svg>
-  );
-}
-
 /** Кастомный выпадающий список в стиле тёмной темы приложения. */
 export function Select<T extends string>(props: SelectProps<T>) {
   const { value, options, onChange, className, disabled, name, ariaLabel } = props;
@@ -70,11 +40,7 @@ export function Select<T extends string>(props: SelectProps<T>) {
         aria-label={ariaLabel}
       >
         <RadixSelect.Value className='select__value'/>
-        <RadixSelect.Icon className='select__icon' asChild>
-          <span>
-            <SelectChevronIcon/>
-          </span>
-        </RadixSelect.Icon>
+        <span className='select__chevron'/>
       </RadixSelect.Trigger>
 
       <RadixSelect.Portal>
@@ -94,9 +60,7 @@ export function Select<T extends string>(props: SelectProps<T>) {
                 <RadixSelect.ItemText className='select__item-text'>
                   {option.label}
                 </RadixSelect.ItemText>
-                <RadixSelect.ItemIndicator className='select__item-indicator'>
-                  <SelectCheckIcon/>
-                </RadixSelect.ItemIndicator>
+                <RadixSelect.ItemIndicator className='select__item-indicator select__check'/>
               </RadixSelect.Item>
             ))}
           </RadixSelect.Viewport>
